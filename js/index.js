9f7d392b0ac10204898248adfd33483b381679c4
TH4YSZ <thainacristina.sz07@gmail.com>
2023-09-26 08:38:18 -0300

    Modules

--- js/index.js
+++ js/index.js
@@ -1,42 +1,6 @@
-const imagens = document.querySelectorAll('.image-list li')
-const textos = document.querySelectorAll('.conteudo section')
+import {ativarConteudo, ativarRespostas, removerRespostas} from "./modulos/acordion.js"
+ativarConteudo()
+ativarRespostas()
+removerRespostas()
 
-
-function ativarConteudo(indice){
-
-    textos.forEach((item)=>{
-        item.classList.remove('active')
-    })
-    textos[indice].classList.add('active')
-}
-
-imagens.forEach((item, indice)=>{
-    item.addEventListener('click', ()=>{
-        ativarConteudo(indice)
-    })
-})
-
-const perguntas = document.querySelectorAll('#faq .perguntas')
-const respostas = document.querySelectorAll('#faq .respostas')
-
-function ativarRespostas(indice){
-    respostas[indice].classList.add('active')
-}
-
-function removerRespostas(indice) {
-    perguntas[indice].addEventListener('click', () => {
-        if (respostas[indice].classList.contains('active')) {
-            respostas[indice].classList.remove('active');
-        }
-        else {
-            respostas[indice].classList.add('active');
-        }
-    });
-}
-perguntas.forEach((item, indice)=>{
-    item.addEventListener('click', ()=>{
-        ativarRespostas(indice)
-        removerRespostas(indice)
-    })
-})
 

af9a8c5aa7954fe5af62a2fe2798642530c3870f
TH4YSZ <thainacristina.sz07@gmail.com>
2023-10-10 10:55:43 -0300

    Modal

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import {ativarConteudo, ativarRespostas, removerRespostas} from "./modulos/acordion.js"
 import {menuMobileInit} from "./modulos/tabMenu.js"
-import { pegarValorForm, exibirModal, ocultarModal } from "./modulos/modal.js"
+import { pegarValorForm, exibirModal,  } from "./modulos/modal.js"
 
 menuMobileInit()
 exibirModal()
--- js/modulos/modal.js
+++ js/modulos/modal.js
@@ -3,6 +3,7 @@ const sucesso = document.querySelector('.msg')
 const dados = {} //Objeto
 
 
+
 export function pegarValorForm(event){
     dados[event.target.name] = event.target.value //Propriedades do objeto armazenanando valores
     console.log(dados)
@@ -27,7 +28,8 @@ form.addEventListener('submit', (event) =>{
     event.preventDefault()
     localStorage.setItem(dados.email, JSON.stringify(dados))
     sucesso.classList.add('active')
-    ocultarModal()
+    setTimeout(ocultarModal, 3000)
+    
     
 
 })

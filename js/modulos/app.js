d14a6993a615e4895ab85bb3716c0c80bc579285
TH4YSZ <thainacristina.sz07@gmail.com>
2023-10-10 08:26:24 -0300

    Formulário

--- js/modulos/app.js
+++ js/modulos/app.js
@@ -1,9 +1,16 @@
 const form = document.querySelector('.form')
+const dados = {} //Objeto
 
 
 export function pegarValorForm(event){
-    console.log(event.target.value)
+    dados[event.target.name] = event.target.value //Propriedades do objeto armazenanando valores
+    console.log(dados)
 }
 
-form.addEventListener('change', pegarValorForm )
 
+form.addEventListener('change', pegarValorForm )
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    localStorage.setItem(dados.email, JSON.stringify(dados))
+    alert("Dados salvos com sucesso!")
+})
